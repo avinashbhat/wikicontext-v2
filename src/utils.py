646705989100bb30c8c558d1db97bc0a5c5d4940
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-10-21 08:16:51 +0530

    Finalised util class methods

--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,13 @@
-import re
 from urllib.parse import unquote
-from urllib.request import urlopen
-
-from bs4 import BeautifulSoup
 
 
-# Text Processing for enforcing encodings
-def cleanupLatinEncoding(word):
-    try:
-        return unquote(word, errors='strict')
-    except UnicodeDecodeError:
-        return unquote(word, encoding='latin-1')
+class Utils():
+    def __init__(self, latin_encoder="latin-1"):
+        self.latin_encoder = latin_encoder
         
-
-def getLinks(url):
-	# Append the url tag to wiki
-	html = urlopen("https://en.wikipedia.org" + url)
-	bsObj = BeautifulSoup(html, "lxml")
-	newLinks = list()
-	# The url tags are always i) found in bodycontent tag
-	for each in bsObj.findAll("div", {"id": "bodyContent"}):
-		for link in bsObj.findAll("a", href=re.compile("^(/wiki/)((?!:).)*$")):
-			if 'href' in link.attrs:
-				stripped = re.sub('/wiki/', "", link.attrs['href'])
-				stripped = re.sub('_', " ", stripped)
-				stripped = cleanupLatinEncoding(stripped)
-				newLinks.append(stripped)
-	return newLinks
+    def _cleanup_latin_encoding(self, word):
+        try:
+            return unquote(word, errors='strict')
+        except UnicodeDecodeError:
+            return unquote(word, encoding=self.latin_encoder)
+        
