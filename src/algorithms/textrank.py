268ab332c1fadb0721eb1e2da28f30dc7aa36506
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-10-21 14:07:56 +0530

    Added TextRank

---
+++ src/algorithms/textrank.py
@@ -0,0 +1,14 @@
+from ..subject import Subject
+
+from gensim.summarization import summarizer
+
+class TextRank():
+    def __init__(self, text, ratio=0.2, word_count=None, split=False):
+        self.text = text
+        self.ratio = ratio
+        self.word_count = word_count
+        self.split = split
+
+    def get_summary(self):
+        return summarizer.summarize(self.text, self.ratio, 
+            self.word_count, self.split)
