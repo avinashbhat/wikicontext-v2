3284906b8bfc30725a7368564fc7bb5493e58691
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-10-21 12:35:46 +0530

    Removed unnecessary method

--- src/subject.py
+++ src/subject.py
@@ -5,28 +5,13 @@ import re
 import wikipedia
 
 
-
 class Subject(Utils):
     def __init__(self, subject, bs_parser="lxml", latin_encoder="latin-1"):
         super().__init__(latin_encoder="latin-1")
         self.subject = subject
         self.bs_parser = bs_parser
-        # This is None to simulate lazy loading
+        # lazy loaded
         self.wikipedia_object = None
-
-    # def get_hyperlinks(self):
-    #     # Append the url tag to wiki
-    #     bs_obj = BeautifulSoup(self._get_html, self.bs_parser)
-    #     new_links = list()
-    #     # The url tags are always i) found in bodycontent tag
-    #     for each in bs_obj.findAll("div", {"id": "bodyContent"}):
-    #         for link in bs_obj.findAll("a", href=re.compile("^(/wiki/)((?!:).)*$")):
-    #             if 'href' in link.attrs:
-    #                 stripped = re.sub('/wiki/', "", link.attrs['href'])
-    #                 stripped = re.sub('_', " ", stripped)
-    #                 stripped = self._cleanup_latin_encoding(stripped)
-    #                 newLinks.append(stripped)
-    #     return new_links
 
     def _get_wiki_object(self):
         if not self.wikipedia_object:
