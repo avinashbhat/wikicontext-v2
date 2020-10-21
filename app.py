2f68eba87d020059e7028e451f028260975cc401
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-10-21 14:39:39 +0530

    Added Cache, and a sample flow for summary

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from src.subject import Subject
+from src.algorithms.textrank import TextRank
 
 import streamlit as st
 
 st.title('WikiContext')
 
+algorithm = st.selectbox("Algorithm that you want to use.", ["TextRank", "T5"], index=0)
+
 subject = st.text_input(label="The Wikipedia page that you want summarized.", 
                     value="")
 
-if subject:
+@st.cache
+def get_textrank_summary(summary):
+    # Move this function to wikicontext file 
+    return TextRank(summary).get_summary()
+
+
+if subject and algorithm:
     subject = Subject(subject)
-    summary = subject._get_summary()
+    if algorithm == "TextRank":
+        summary = subject._get_summary()
+        summary = get_textrank_summary(summary)
+    else:
+        summary = "Algorithm is not implemented.."
     st.markdown(summary)
-    # st.markdown(page)
