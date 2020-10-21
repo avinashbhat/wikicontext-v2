63d6995429231145ee2b90841002e8f27eb92234
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-10-21 13:18:48 +0530

    Added the wikicontext main class

---
+++ src/wikicontext.py
@@ -0,0 +1,9 @@
+class WikiContext():
+    def __init__(self):
+        pass
+
+# The actual process has to be mapped here, now this is done in the app.py file itself
+# 1) Get the data for the subject
+# 2) Get the hyperlinks
+# 3) Get the most relevant keywords (using RAKE or any other algo)
+# 4) Get the summary for the individual hyperlinks
