189753eec188ba7a333e35005bb14c2a91f49b0a
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-10-29 14:22:00 +0530

    Updated algo classes to contain get_summary methods

--- src/algorithms/textrank.py
+++ src/algorithms/textrank.py
@@ -4,11 +4,13 @@ from gensim.summarization import summarizer
 
 
 class TextRank():
-    def __init__(self, text, ratio=0.2, word_count=None, split=False):
+    def __init__(self, text, ratio=0.2, word_count=None, 
+            split=False):
         self.text = text
         self.ratio = ratio
         self.word_count = word_count
         self.split = split
 
     def get_summary(self):
-        return summarizer.summarize(self.text, self.ratio, self.word_count, self.split)
+        return summarizer.summarize(self.text, self.ratio, 
+            self.word_count, self.split)
