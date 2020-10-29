bebb07425f64e755fb616482160020ae3e1a1d92
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-10-29 14:23:29 +0530

    Updated wikicontext to adhere better caching

--- src/wikicontext.py
+++ src/wikicontext.py
@@ -3,29 +3,50 @@ from src.algorithms.textrank import TextRank
 from src.algorithms.transformer import Transformer
 
 class WikiContext(Subject):
-    def __init__(self, subject, algorithm, max_prereq=5):
+    def __init__(self, subject, algorithm, params, max_prereq=5):
         Subject.__init__(self, subject=subject)
         self.algorithm = algorithm
+        self.params = params
         self.max_prereq = max_prereq
+        self.content = None
+        self.prereq = {}
 
-    def get_main_summary(self):
-        return self._get_summary()
-        # return self._get_content()
+    def get_main_content(self):
+        self.content = self._get_content()
 
-    def get_prereq_summary(self):
+    def get_prereqs_content(self):
         keyphrases = self.get_top_keywords_from_rake()
         hyperlinks = self._get_links()
         match = []
         for phrase in keyphrases:
-            match.extend([hyperlink for hyperlink in hyperlinks if hyperlink.lower() in phrase.lower()])
+            match.extend([hyperlink for hyperlink in hyperlinks if 
+                hyperlink.lower() in phrase.lower()])
             # Make it unique
             match = list(set(match))
             if len(match) >= self.max_prereq:
                 break
         
-        pre_requisites = {}
         for prereq in match:
             s = Subject(prereq)
-            pre_requisites[prereq] = s._get_summary()
-        
-        return pre_requisites
+            self.prereq[prereq] = s._get_summary()
+    
+    def mapper(self):
+        if self.algorithm == 'TextRank':
+            return TextRank
+        elif self.algorithm == 'BART':
+            return Transformer
+        elif self.algorithm == 'T5':
+            return Transformer
+
+    def get_main_summary(self):
+        model_class = self.mapper()
+        model = model_class(self.content, **self.params)
+        return model.get_summary(self.algorithm)
+
+    def get_prereqs_summary(self):
+        model_class = self.mapper()
+        prereq_summary = {}
+        for each in self.prereq:
+            model = model_class(self.prereq[each], **self.params)
+            prereq_summary[each] = model.get_summary(self.algorithm)
+        return prereq_summary
