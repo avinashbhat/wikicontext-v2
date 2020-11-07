5b367652d4074aa2832864787403673adc50389b
avinashbhat <avinashbhatneelavar@googlemail.com>
2020-11-07 16:47:07 +0530

    Fixes for transformer class

--- src/algorithms/transformer.py
+++ src/algorithms/transformer.py
@@ -1,24 +1,37 @@
 from transformers import pipeline
+from transformers import AutoTokenizer, AutoModel, AutoModelForSeq2SeqLM
 
 
 class Transformer:
-    def __init__(self, text, model='t5-small', tokenizer='t5-small', min_length=40, max_length=150, framework='tf'):
+    def __init__(self, text, model='t5-small', tokenizer='t5-small', min_length=20, 
+            max_length=80, framework='pt'):
         self.text = text
         self.model = model
+        self.device = -1 # no cuda support
         self.tokenizer = tokenizer
         self.min_length = min_length
         self.max_length = max_length
         self.framework = framework
 
+    def _get_pipeline(self, model, tokenizer):
+        return pipeline("summarization", model=self.model, tokenizer=self.tokenizer, 
+                    device=self.device, framework=self.framework)
+
     def _get_bart_summary(self):
-        summarization = pipeline("summarization")
-        summary = summarization(self.text, min_length=self.min_length, max_length=self.max_length)[0]['summary_text']
-        return summary
+        tokenizer = AutoTokenizer.from_pretrained(self.tokenizer)
+        model = AutoModel.from_pretrained(self.model)
+        pipeline = self._get_pipeline(model, tokenizer)
+        summary = pipeline(self.text, min_length=self.min_length, 
+                max_length=self.max_length)
+        return summary[0]['summary_text']
 
     def _get_t5_summary(self):
-        summarization = pipeline("summarization", model=self.model, tokenizer=self.tokenizer, framework=self.framework)
-        summary = summarization(self.text, min_length=self.min_length, max_length=self.max_length)[0]['summary_text']
-        return summary
+        tokenizer = AutoTokenizer.from_pretrained(self.tokenizer)
+        model = AutoModelForSeq2SeqLM.from_pretrained(self.model)
+        pipeline = self._get_pipeline(model, tokenizer)
+        summary = pipeline(self.text, min_length=self.min_length, 
+                max_length=self.max_length)
+        return summary[0]['summary_text']
 
     def get_summary(self, algorithm):
         if algorithm == 'BART':
