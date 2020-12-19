1aec6b208ac102d7c1d413a81b7577083b28d037
Nihal D'Souza <nidsouza@cisco.com>
2020-12-19 19:15:15 +0530

    Removing t5 related UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from src.wikicontext import WikiContext
-import json
-import os, urllib
+import wikipedia
 
 
 def main():
@@ -19,20 +18,23 @@ def run_the_app(algorithm, params=None):
     if params is None:
         params = {}
 
-    subject = st.text_input(label="The Wikipedia page that you want summarized.", value="")
+    query = st.text_input(label="The topic you want summarized.", value="")
+    results = []
+    if query:
+        results = wikipedia.search(query)
+        subject = st.selectbox(label="Choose the Wikipedia page.", options=results, index=0)
 
-    if subject and algorithm:
-        wc = WikiContext(subject, algorithm, params)
-        st.title(subject)
-        with st.spinner(text="Generating summary..."):
-            st.markdown(get_main_summary(wc))
-        with st.spinner(text="Generating prerequisites..."):
-            prereqs = get_prereq_summary(wc)
-        st.header("Prerequisites")
-        for subhead in prereqs:
-            st.subheader(subhead)
-            st.write(prereqs[subhead])
-        st.success(text="Prerequisites generated successfully")
+        if subject and algorithm:
+            wc = WikiContext(subject, algorithm, params)
+            st.title(subject)
+            with st.spinner(text="Generating summary..."):
+                st.markdown(get_main_summary(wc))
+            with st.spinner(text="Generating prerequisites..."):
+                prereqs = get_prereq_summary(wc)
+            st.header("Prerequisites")
+            for subhead in prereqs:
+                st.subheader(subhead)
+                st.write(prereqs[subhead])
 
 
 @st.cache(show_spinner=False)
